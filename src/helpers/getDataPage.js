549067fbe00c8c680da5bc588058efe5c6914df0
Santiago Rochi <santiagorochi9@gmail.com>
2021-11-10 06:43:48 -0300

    actualized offset

--- src/helpers/getDataPage.js
+++ src/helpers/getDataPage.js
@@ -1,17 +1,16 @@
 
-
+rojo, azulao, azul, mas azul, morado
 const getDataPage = (images, offset, limit ) => {
        const size=images.length
        let previous='';
        let next='';
-
-       offset<limit?prevOffset=0:prevOffset=offset;
+       offset<limit?prevOffset=0:prevOffset=parseInt(offset)-10;
 
        (parseInt(offset)+parseInt(limit))>=(size)
               ?next=null
               :next=`${process.env.API_URL}/photos?offset=${(parseInt(offset)+parseInt(limit))}&limit=${limit}`;
 
-       prevOffset==0
+       prevOffset<0
               ?previous=null
               :previous=`${process.env.API_URL}/photos?offset=${prevOffset}&limit=${limit}`;
 
